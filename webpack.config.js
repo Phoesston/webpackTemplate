d245001de8cd4b082d78d23a66a7b134cb1c1697
Joshua <71791590+Phoesston@users.noreply.github.com>
2025-11-02 19:23:20 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/scripts/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath:"",
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    watchFiles: ["scripts/**/*", "styles/**/*", "src/**/*"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
